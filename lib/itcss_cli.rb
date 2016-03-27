c75b43eb9c5a349595fbda1af641f137f43f6fa1
Kande Bonfim <kandebonfim@gmail.com>
2016-03-27 03:50:26 -0300

    Fragment methods

--- lib/itcss_cli.rb
+++ lib/itcss_cli.rb
@@ -7,28 +7,52 @@ module ItcssCli
 
     ITCSS_DIR = "stylesheets"
     ITCSS_FILE_TEMPLATE = File.expand_path(File.join(File.dirname(__FILE__), "../templates/itcss_file.erb"))
+    ITCSS_FILES = [
+      "settings",
+      "tools",
+      "generic",
+      "base",
+      "objects",
+      "components",
+      "trumps"
+    ]
 
-    def command
-      ARGV.each do |arg|
+    def command_parser
+      if ARGV[0] == 'init'                                   # $ itcss init
 
-        File.open ITCSS_FILE_TEMPLATE do |io|
-          template = ERB.new io.read
-
-          files = {
-            "settings" => "#settings.example",
-            "components" => "#components.example"
-          }
-
-          FileUtils.mkdir_p ITCSS_DIR
-
-          files.each do |file, contents|
-            File.open "#{ITCSS_DIR}/#{file}.#{file}.sass", "w+" do |out|
-              out.puts template.result binding
-            end
-          end
-        end
-
+      elsif ARGV[0] == 'install' && ARGV[1] == 'example'     # $ itcss install example
+        new_itcss_basic_structure
+      elsif ARGV[0] == 'new' && ARGV[1] && ARGV[2]           # $ itcss new components buttons
+        new_itcss_module(ARGV[1], ARGV[2])
       end
     end
+
+    def new_itcss_basic_structure
+      File.open ITCSS_FILE_TEMPLATE do |io|
+        template = ERB.new io.read
+
+        ITCSS_FILES.each do |file|
+          new_itcss_file(file, 'example', template)
+        end
+      end
+    end
+
+    def new_itcss_module(type, file)
+      File.open ITCSS_FILE_TEMPLATE do |io|
+        template = ERB.new io.read
+        new_itcss_file(type, file, template)
+      end
+    end
+
+    def new_itcss_file(type, file, template)
+      FileUtils.mkdir_p ITCSS_DIR
+      FileUtils.mkdir_p "#{ITCSS_DIR}/#{type}"
+
+      contents = "##{type}.#{file}"
+      File.open "#{ITCSS_DIR}/#{type}/_#{type}.#{file}.sass", "w+" do |out|
+        out.puts template.result binding
+      end
+    end
+
   end
 end
