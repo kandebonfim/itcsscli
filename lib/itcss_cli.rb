82d810cb06a8f8e8f9ae5382bd635de22abbab54
Kande Bonfim <kandebonfim@gmail.com>
2016-03-27 01:46:09 -0300

    Enable code generation via cli

--- lib/itcss_cli.rb
+++ lib/itcss_cli.rb
@@ -1,5 +1,33 @@
 require "itcss_cli/version"
+require "erb"
+require 'fileutils'
 
 module ItcssCli
-  # Your code goes here...
+  class Init
+
+    ITCSS_DIR = 'stylesheets'
+
+    def command
+      ARGV.each do |arg|
+
+        File.open("templates/itcss_file.erb") do |io|
+          template = ERB.new io.read
+
+          files = {
+            "settings" => "#settings.example",
+            "components" => "#components.example"
+          }
+
+          FileUtils.mkdir_p ITCSS_DIR
+
+          files.each do |file, contents|
+            File.open "#{ITCSS_DIR}/#{file}.#{file}.sass", "w+" do |out|
+              out.puts template.result binding
+            end
+          end
+        end
+
+      end
+    end
+  end
 end
